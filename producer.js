0de560620f8837c85738d734d8efa1d554fb8448
Dev Patel <dvptl68@gmail.com>
2021-11-16 20:41:50 -0500

    Test youtube lib

--- producer.js
+++ producer.js
@@ -2,6 +2,15 @@ const { Client, Intents } = require('discord.js');
 const ytdl = require('ytdl-core');
 const { prefix, token } = require('./config.json');
 
+const getInfo = async () => {
+  const songLink = 'https://www.youtube.com/watch?v=iI34LYmJ1Fs';
+  const songInfo = await ytdl.getInfo(songLink);
+  console.log(`title: ${songInfo.videoDetails.title}\nurl: ${songInfo.videoDetails.video_url}`);
+};
+
+getInfo();
+
+/*
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.login(token);
 
@@ -16,8 +25,8 @@ client.on('messageCreate', async message => {
   const content = message.content.toLowerCase();
 
   if (!content.startsWith(prefix)) return;
-
-  if (content.startsWith(`${prefix}play`)) message.channel.send('Playing music.');
+  else if (content.startsWith(`${prefix}play`)) message.channel.send('Playing music.');
   else if (content.startsWith(`${prefix}stop`)) message.channel.send('Stopping music.');
   else message.channel.send('Not a valid command.');
 });
+*/
