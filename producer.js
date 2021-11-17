4aeec88a586a4b2514b4c15c638579023f037c02
Dev Patel <dvptl68@gmail.com>
2021-11-16 19:49:09 -0500

    Fix js

--- producer.js
+++ producer.js
@@ -1,3 +1,10 @@
-const discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const ytdl = require('ytdl-core');
+const { prefix, token } = require('./config.json');
 
-console.log('hello');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.login(token);
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
