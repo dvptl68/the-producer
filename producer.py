476c330d325cbb2c4cf4e78bc612b152e4f80a5d
Dev Patel <dvptl68@gmail.com>
2021-11-16 19:34:11 -0500

    Import js

--- producer.py
+++ producer.py
@@ -1,9 +1,10 @@
 import os
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')    
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
